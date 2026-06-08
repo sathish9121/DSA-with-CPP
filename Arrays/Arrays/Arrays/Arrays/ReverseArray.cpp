15d49cf57f1206b0dadad42d0b82108afbe680d1
sathish  jalli <ssathish93622@gmail.com>
2026-06-08 16:03:41 +0530

    Create ReverseArray.cpp

---
+++ Arrays/Arrays/Arrays/Arrays/ReverseArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    cout << "Reversed Array: ";
+
+    for(int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
